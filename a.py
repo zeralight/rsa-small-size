c14798fff3e03d1ed067a4d456dccf98b7aa101f
zeralight <hedi2kanon@hotmail.com>
2018-05-08 11:33:47 +0200

    cp

--- a.py
+++
@@ -1,9 +0,0 @@
-
-from Crypto.Cipher import PKCS1_OAEP
-from Crypto.PublicKey import RSA
-
-message = b'I wonder if it will work'
-key = RSA.importKey(open('private.pem').read())
-cipher = PKCS1_OAEP.new(key)
-ciphertext = cipher.encrypt(message)
-print(ciphertext)
